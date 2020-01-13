49942362de7abdee8c09cc4f53dbea7f0737b7d2
cirlceLime <57714545+cirlceLime@users.noreply.github.com>
2020-01-13 22:09:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,61 +1,24 @@
-
 // Импортируем другие js-файлы
+$(document).ready(function() {
+  const toggler = $(".toggle-link");
+  const toggleBlock = $(".toggle-block");
+  $(toggler).click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(this)
+      .siblings(toggleBlock)
+      .toggleClass("active");
+  });
+});
 
-
-$(document).ready(function(){
-  $('.menu__triger').click(function(){
-    $('#main_nav').toggleClass('open');
-  });
-  $('.toggleSidebar').click(function(){
-    $('.ant-drawer-left').show();
-  });
-  $('.ant-drawer-mask').click(function(){
-    $('.ant-drawer').hide();
-  });
-  $('.Filter_Icon').click(function(){
-    $('.ant-drawer-right').show();
-    console.log($('.ant-drawer-right'));
-  });
-  $('#grid_view').click(function(){
-    $(this).addClass('active');
-    $('#list_view').removeClass('active');
-    $('#product_view_block').removeClass('list_view');
-  });
-  $('#list_view').click(function(){
-    $(this).addClass('active');
-    $('#grid_view').removeClass('active');
-    $('#product_view_block').addClass('list_view');
-  });
-});
-function viewList(){
-    document.getElementById('product_view_block').classList.add('list_view');
-    document.getElementById('grid_view').classList.remove('active');
-    document.getElementById('list_view').classList.add('active');
+const burgerButton = document.querySelector(".mobile-menu");
+let menuStatus = false;
+burgerButton.addEventListener("click", () => {
+  if (!menuStatus) {
+    burgerButton.classList.add("active");
+    menuStatus = true;
+  } else {
+    burgerButton.classList.remove("active");
+    menuStatus = false;
   }
-  function viewGrid(){
-  document.getElementById('product_view_block').classList.remove('list_view');
-    document.getElementById('list_view').classList.remove('active');
-    document.getElementById('grid_view').classList.add('active');;
-  }
-$('.anticon-plus').click(function(){
-    let value = $(this).parent().siblings('#item_count').html();
-    console.log(value);
-    value = parseInt(value) + 1;
-    $(this).parent().siblings('#item_count').html(value);
-});
-$('.anticon-minus').click(function(){
-  let value = $('#item_count').html();
-  
-  if(value == '0'){
-    value = 0;
-  }else{
-    value = parseInt(value) - 1;
-  }
-  $('#item_count').html(value);
-});
-$('.Payment').click(function(){
-  $('.Payment').removeClass('__Active');
-  $('.Payment').addClass('false');
-  $(this).addClass('__Active');
-  $(this).removeClass('false');  
 });
