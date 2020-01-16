3ce11d411563ecd4228cbc8e0d55dc1b78290bb1
cirlceLime <57714545+cirlceLime@users.noreply.github.com>
2020-01-16 19:18:58 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,9 +8,22 @@ $(document).ready(function() {
     $(this)
       .siblings(toggleBlock)
       .toggleClass("active");
+    const toggleHeight = $(this)
+      .siblings(toggleBlock)
+      .height();
+    if ($(this).hasClass("active")) {
+      $(this)
+        .parent()
+        .next()
+        .css("margin-top", toggleHeight + 20);
+    } else {
+      $(this)
+        .parent()
+        .next()
+        .css("margin-top", "10px");
+    }
   });
 });
-
 const burgerButton = document.querySelector(".mobile-menu");
 let menuStatus = false;
 burgerButton.addEventListener("click", () => {
@@ -22,3 +35,61 @@ burgerButton.addEventListener("click", () => {
     menuStatus = false;
   }
 });
+const reviewMark = document.querySelectorAll(".reviews__mark .mark");
+let sum = 0;
+reviewMark.forEach(element => {
+  if (element.innerHTML >= 4 && element.innerHTML < 5) {
+    element.classList.add("high");
+  }
+  if (element.innerHTML >= 3 && element.innerHTML < 4) {
+    element.classList.add("medium");
+  }
+  if (element.innerHTML >= 5) {
+    element.classList.add("awesome");
+  }
+  sum += +element.innerHTML / reviewMark.length;
+});
+document.querySelector(".result-mark").innerHTML = sum.toFixed(2);
+let reviewList = document.querySelectorAll(".reviews__item");
+reviewList.forEach(function(item, i) {
+  console.log(i);
+  if (i < 3) {
+    item.classList.add("active");
+    console.log("123");
+  }
+});
+document.querySelector(".more_link a").addEventListener("click", function(e) {
+  e.preventDefault();
+  let activeReviews = document.querySelectorAll(".reviews__item.active");
+  let lastReview = activeReviews[activeReviews.length - 1];
+  lastReview.nextElementSibling.classList.add("active");
+  let hiddenReview = document
+    .querySelector(".reviews__item")
+    .classList.contains("active");
+  console.log(hiddenReview);
+  if (!hiddenReview) {
+    console.log("123");
+  }
+});
+/*
+const reviewDesk = document.querySelectorAll(".reviews__desc p");
+const reviewLenght = 150;
+reviewDesk.forEach(function(item, i) {
+  reviewDeskText = item.innerHTML;
+  console.log(reviewDeskText);
+  let sliced = reviewDeskText.slice(0, 150);
+  if (reviewDeskText.length > reviewLenght) {
+    sliced += "...";
+    item.innerHTML = sliced;
+  }
+  item.addEventListener("click", function() {
+    this.classList.toggle("active");
+    if (item.classList.contains("active")) {
+      this.innerHTML = reviewDeskText;
+    } else {
+      sliced += "...";
+      item.innerHTML = sliced;
+    }
+  });
+});
+*/
