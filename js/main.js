ef6ec49b5f4111bd443e1597e9262865edd0df78
cirlceLime <57714545+cirlceLime@users.noreply.github.com>
2020-04-05 18:47:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 // Импортируем другие js-файлы
+$(document).ready(function() {
+    const navToggle = $('.nav-toggle'),
+        navList = $('.nav-list'),
+        nav = $('.main-nav');
+
+    $(navToggle).click(function(e) {
+        e.preventDefault()
+        $(this).toggleClass('active');
+        $(navList).toggleClass('active');
+    });
+
+    const $page = $('html, body');
+    $('.main-nav a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 60
+        }, 500);
+        return false;
+    });
+    $(document).scroll(function() {
+        if ($(window).scrollTop() > 70) {
+            $(nav).addClass('active');
+        } else {
+            $(nav).removeClass('active');
+        }
+    });
+});
